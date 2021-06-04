3a6436ab19e3bd8d006c721664817e4f04e8ad8b
HarshDhussa <46370093+HarshDhussa1008@users.noreply.github.com>
2021-06-04 17:24:33 +0530

    Update settings.py

--- whiz/settings.py
+++ whiz/settings.py
@@ -145,9 +145,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 AUTHENTICATION_BACKENDS = (
