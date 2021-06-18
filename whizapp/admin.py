63a41d266d4c893fffd3be61702852144f2ae591
HarshDhussa <46370093+HarshDhussa1008@users.noreply.github.com>
2021-06-18 19:34:25 +0530

    Update admin.py

--- whizapp/admin.py
+++ whizapp/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import Question,Comment,CommentImages,QuestionImages,CommentReply
+from .views import notify
 # Register your models here.
 
-admin.site.register(Question)
+def user_notify(modeladmin, request, queryset):
+    u=User.objects.all()
+    for i in u:
+      for q in queryset:
+        notify(i,q)
+    
+make_published.short_description = "Notify"
+
+class QuestionAdmin(admin.ModelAdmin):
+  actions=[user_notify]
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(QuestionImages)
 admin.site.register(Comment)
 admin.site.register(CommentImages)
